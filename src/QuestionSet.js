e77c4446dc82c311b4fc754d94d9d90865698cf5
Lena Sokolova <sokolhelena94@gmail.com>
2022-09-24 14:28:19 +0200

    refactor: add const styles to change  background when the div isChosen

--- src/QuestionSet.js
+++ src/QuestionSet.js
@@ -1,18 +1,22 @@
 export default function QuestionSet(props) {
+  const styles = {
+    backgroundColor: props.isChosen ? "#D6DBF5" : "transparent",
+  };
+
   return (
     <section className="quest" key={props.id}>
       <p>{props.question}</p>
       <section className="answer-container">
-        <div className="answer-div">
+        <div className="answer-div" style={styles}>
           <p>{props.answer_corr}</p>
         </div>
-        <div className="answer-div">
+        <div className="answer-div" style={styles}>
           <p>{props.inc_answ_one}</p>
         </div>
-        <div className="answer-div">
+        <div className="answer-div" style={styles}>
           <p>{props.inc_answ_two}</p>
         </div>
-        <div className="answer-div">
+        <div className="answer-div" style={styles}>
           <p>{props.inc_answ_three}</p>
         </div>
       </section>
