986e5adcd597eb02079196523d507b780ff1b662
Lena Sokolova <sokolhelena94@gmail.com>
2022-09-25 13:04:20 +0200

    refactor: erase unused style const and part of Answer component

--- src/QuestionSet.js
+++ src/QuestionSet.js
@@ -1,25 +1,7 @@
 export default function QuestionSet(props) {
-  const styles = {
-    backgroundColor: props.isChosen ? "#D6DBF5" : "transparent",
-  };
-
   return (
     <section className="quest" key={props.id}>
       <p>{props.question}</p>
-      <section className="answer-container">
-        <div className="answer-div" style={styles}>
-          <p>{props.answers[3].answer}</p>
-        </div>
-        <div className="answer-div" style={styles}>
-          <p>{props.answers[1].answer}</p>
-        </div>
-        <div className="answer-div" style={styles}>
-          <p>{props.answers[2].answer}</p>
-        </div>
-        <div className="answer-div" style={styles}>
-          <p>{props.answers[0].answer}</p>
-        </div>
-      </section>
     </section>
   );
 }
