130f4313afa0e64dbde0e383f312177965b462a0
Lena Sokolova <sokolhelena94@gmail.com>
2022-09-25 13:02:28 +0200

    feat: create Answers component

---
+++ src/Answers.js
@@ -0,0 +1,41 @@
+export default function Answers(props) {
+  const styles = {
+    backgroundColor: props.isChosen ? "#D6DBF5" : "transparent",
+  };
+  return (
+    <section className="answer-container">
+      <div
+        className="answer-div"
+        style={styles}
+        id={props.answers[3].id}
+        onClick={props.holdAnswer}
+      >
+        <p>{props.answers[3].answer}</p>
+      </div>
+      <div
+        className="answer-div"
+        style={styles}
+        id={props.answers[1].id}
+        onClick={props.holdAnswer}
+      >
+        <p>{props.answers[1].answer}</p>
+      </div>
+      <div
+        className="answer-div"
+        style={styles}
+        id={props.answers[2].id}
+        onClick={props.holdAnswer}
+      >
+        <p>{props.answers[2].answer}</p>
+      </div>
+      <div
+        className="answer-div"
+        style={styles}
+        id={props.answers[0].id}
+        onClick={props.holdAnswer}
+      >
+        <p>{props.answers[0].answer}</p>
+      </div>
+    </section>
+  );
+}
