96d984e550e4614958c5e39f51b0f37f43a07e73
Lena Sokolova <sokolhelena94@gmail.com>
2022-09-27 18:29:01 +0200

    refactor: use ternary operator shows the question set, fetch method as async/await +handle errors

--- src/App.js
+++ src/App.js
@@ -1,24 +1,111 @@
 import { useState } from "react";
-import Questions from "./Questions";
+import QuestionSet from "./QuestionSet";
+import Answers from "./Answers";
+import { nanoid } from "nanoid";
 
 function App() {
   const [isQuesLoaded, setIsQuesLoaded] = useState(false);
+  const [questions, setQuestions] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
 
-  function handleClick() {
-    return setIsQuesLoaded(!isQuesLoaded);
+  async function startQuiz() {
+    try {
+      setIsQuesLoaded(!isQuesLoaded);
+      const response = await fetch(
+        "https://opentdb.com/api.php?amount=5&category=12&difficulty=easy&type=multiple"
+      );
+      const data = await response.json();
+      const allQuestions = data.results;
+      const listOfQuestions = allQuestions.map((item) => {
+        const allAnswers = [
+          {
+            id: nanoid(),
+            isCorrect: false,
+            isChosen: false,
+            answer: item.incorrect_answers[0],
+          },
+          {
+            id: nanoid(),
+            isCorrect: false,
+            isChosen: false,
+            answer: item.incorrect_answers[1],
+          },
+          {
+            id: nanoid(),
+            isCorrect: false,
+            isChosen: false,
+            answer: item.incorrect_answers[2],
+          },
+          {
+            id: nanoid(),
+            isCorrect: true,
+            isChosen: false,
+            answer: item.correct_answer,
+          },
+        ];
+        return {
+          id: nanoid(),
+          question: item.question,
+          answers: allAnswers,
+        };
+      });
+      setQuestions(listOfQuestions);
+    } catch (err) {
+      setError(err.message);
+    } finally {
+      setLoading(false);
+    }
   }
+
+  function holdAnswer(id) {
+    console.log(id);
+    setQuestions((prevQuestion) =>
+      prevQuestion.map((question) =>
+        question.answers.id === id
+          ? {
+              ...question,
+              answers: question.answers.map((answer) =>
+                answer.id === id
+                  ? { ...answer, isChosen: !answer.isChosen }
+                  : answer
+              ),
+            }
+          : question
+      )
+    );
+  }
+
+  const questionElm = questions.map((question, index) => {
+    return (
+      <section key={index}>
+        <QuestionSet question={question.question} key={question.id} />
+        <Answers
+          answers={question.answers}
+          isChosen={question.answers.isChosen}
+          holdAnswer={holdAnswer}
+        />
+      </section>
+    );
+  });
 
   return (
     <div className="App">
-      {isQuesLoaded ? (
-        <Questions />
-      ) : (
+      {!isQuesLoaded ? (
         <main>
           <h1 className="title-app">Quizzical</h1>
           <p className="desc-app">Some description if needed</p>
-          <button className="btn" onClick={handleClick}>
+          <button className="btn" onClick={startQuiz}>
             Start Quiz
           </button>
+        </main>
+      ) : (
+        <main className="quest-box">
+          {loading && <div>Loading data...</div>}
+          {error && <div>{`There is a problem fetchning data = ${error}`}</div>}
+          <section className="quest-content">{questionElm}</section>
+
+          <button className="answer-btn">Check Answers</button>
         </main>
       )}
     </div>
