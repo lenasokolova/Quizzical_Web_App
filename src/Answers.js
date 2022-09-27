543c07aa52d30f86bcb4d8978719f839355d2655
Lena Sokolova <sokolhelena94@gmail.com>
2022-09-27 18:27:13 +0200

    refactor: fix holdAnswer() in onClick event

--- src/Answers.js
+++ src/Answers.js
@@ -1,16 +1,15 @@
 export default function Answers(props) {
   const styles = {
-    backgroundColor: props.isChosen ? "#D6DBF5" : "transparent",
+    backgroundColor: props.answers.isChosen ? "#D6DBF5" : "transparent",
   };
 
   return (
     <section className="answer-container">
-      {/* {answerOptions} */}
       <div
         className="answer-div"
         style={styles}
         id={props.answers[3].id}
-        onClick={props.holdAnswer}
+        onClick={() => props.holdAnswer(props.answers[3].id)}
       >
         <p>{props.answers[3].answer}</p>
       </div>
@@ -18,7 +17,7 @@ export default function Answers(props) {
         className="answer-div"
         style={styles}
         id={props.answers[1].id}
-        onClick={props.holdAnswer}
+        onClick={() => props.holdAnswer(props.answers[1].id)}
       >
         <p>{props.answers[1].answer}</p>
       </div>
@@ -26,7 +25,7 @@ export default function Answers(props) {
         className="answer-div"
         style={styles}
         id={props.answers[2].id}
-        onClick={props.holdAnswer}
+        onClick={() => props.holdAnswer(props.answers[2].id)}
       >
         <p>{props.answers[2].answer}</p>
       </div>
@@ -34,7 +33,7 @@ export default function Answers(props) {
         className="answer-div"
         style={styles}
         id={props.answers[0].id}
-        onClick={props.holdAnswer}
+        onClick={() => props.holdAnswer(props.answers[0].id)}
       >
         <p>{props.answers[0].answer}</p>
       </div>
